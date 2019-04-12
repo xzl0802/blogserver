f72b8329e5bd86d41018d369bd6a4e05bb74f9d5
xzl <xzlperfect@163.com>
2019-04-12 17:00:43 +0800

    新增mysql 连接配置

--- config/config.default.js
+++ config/config.default.js
@@ -5,13 +5,13 @@
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
+
 module.exports = appInfo => {
   /**
    * built-in config
    * @type {Egg.EggAppConfig}
    **/
   const config = {};
-
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1555057030501_9726';
 
--- config/plugin.js
+++ config/plugin.js
@@ -3,7 +3,9 @@
 /** @type Egg.EggPlugin */
 module.exports = {
   // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
+  //mysql
+  mysql : {
+    enable: true,
+    package: 'egg-mysql',
+  }
 };
