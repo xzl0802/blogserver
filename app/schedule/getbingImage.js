0b6866a4d190199333dea26035691bee600b26e9
xzl <xzlperfect@163.com>
2019-04-28 16:38:12 +0800

    新增文件

---
+++ app/schedule/getbingImage.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: xzl 
+ * @Date: 2019-04-28 15:59:30 
+ * @Last Modified by: xzl
+ * @Last Modified time: 2019-04-28 16:00:04
+ */
+ //获取bing 每日图片
+
+ 
---
+++ app/service/index.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: xzl 
+ * @Date: 2019-04-28 15:57:33 
+ * @Last Modified by:   xzl 
+ * @Last Modified time: 2019-04-28 15:57:33 
+ */
