4f29fb270f3f2f7bf42b683558ed47752f4a5ce7
xzl <xzlperfect@163.com>
2019-04-29 14:59:22 +0800

    debug

--- app/schedule/getbingImage.js
+++ app/schedule/getbingImage.js
@@ -2,8 +2,16 @@
  * @Author: xzl 
  * @Date: 2019-04-28 15:59:30 
  * @Last Modified by: xzl
- * @Last Modified time: 2019-04-28 16:00:04
+ * @Last Modified time: 2019-04-29 13:55:51
  */
  //获取bing 每日图片
 
- 
+ module.exports = {
+    schedule: {
+      interval: '1h', // 1 分钟间隔
+      type: 'all', // 指定所有的 worker 都需要执行
+    },
+    async task(ctx) {
+    console.log(ctx,'12')
+    },
+  };
