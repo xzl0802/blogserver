afd50c7c15e22a5c135f10f31758d3635a170969
xzl <xzlperfect@163.com>
2019-05-08 15:07:10 +0800

    相关修改

--- app/schedule/getImg.js
+++ app/schedule/getImg.js
@@ -2,7 +2,7 @@
  * @Author: xzl 
  * @Date: 2019-05-08 10:16:30 
  * @Last Modified by: xzl
- * @Last Modified time: 2019-05-08 14:44:22
+ * @Last Modified time: 2019-05-08 15:06:37
  */
  const puppeteer  = require("puppeteer");
  const cheerio  = require("cheerio");
@@ -23,10 +23,11 @@
     }))
      
      let page = await browser.newPage();
+    
      await page.goto('http://www.wjlibw.space/gxew_6.html');  //371
      let content = await page.content();
-     let dTitleHandle = await page.$('.mainArea');  // 使用css selector格式查找商品名称，返回
-     let dTitle = await page.evaluate(dTitle => dTitle.innerHTML, dTitleHandle); // 获取dom innerHTML
+     let dTitleHandle = await page.$('.mainArea'); 
+     let dTitle = await page.evaluate(dTitle => dTitle.innerHTML, dTitleHandle); 
      let $ =cheerio.load(dTitle);
 //    $("li a ").each(function(i, e) {
 //        let url = $(e).attr("href");
