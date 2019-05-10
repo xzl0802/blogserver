255f0746581d9890e33a2d6963946a0d4bbdd117
xzl <xzlperfect@163.com>
2019-05-10 14:24:29 +0800

    修改路由

--- app/controller/login.js
+++ app/controller/login.js
@@ -1,17 +1,17 @@
 /*
  * @Author: xzl 
  * @Date: 2019-04-30 10:05:18 
- * @Last Modified by:   xzl 
- * @Last Modified time: 2019-04-30 10:05:18 
+ * @Last Modified by: xzl
+ * @Last Modified time: 2019-05-10 14:22:55
  */
 
 const Controller = require('egg').Controller;
 
-class HomeController extends Controller {
-  async index() {
+class LoginController extends Controller {
+  async loginIndex() {
     const { ctx } = this;
     ctx.body = 'hi, egg';
   }
 }
 
-module.exports = HomeController;
+module.exports = LoginController;
--- app/router.js
+++ app/router.js
@@ -6,11 +6,8 @@
 
 module.exports = app => {
   const { router, controller } = app;
+  
   router.get('/', controller.home.index);
+  router.get('/login',controller.login.loginIndex);
 };
 
-//登录接口
-module.exports =app =>{
-  const {router, controller} =app;
-  router.get('/login',controller.login.index);
-}
