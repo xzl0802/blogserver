14c974317a87b291200f228caada409b6a7871fb
xzl <xzlperfect@163.com>
2019-05-31 10:55:19 +0800

    解决冲突

--- app/schedule/getImg.js
+++ app/schedule/getImg.js
@@ -2,7 +2,7 @@
  * @Author: xzl 
  * @Date: 2019-05-08 10:16:30 
  * @Last Modified by: xzl
- * @Last Modified time: 2019-05-31 10:46:59
+ * @Last Modified time: 2019-05-31 10:54:55
  */
  const puppeteer  = require("puppeteer");
  const cheerio  = require("cheerio");
@@ -27,24 +27,12 @@
     }))
      
      let page = await browser.newPage();
-<<<<<<< HEAD
     
      await page.goto('http://www.baidu.com');  //371
      let content = await page.content();
      let dTitleHandle = await page.$('.mainArea'); 
      let dTitle = await page.evaluate(dTitle => dTitle.innerHTML, dTitleHandle); 
      let $ =cheerio.load(dTitle);
-=======
-     var  url;
-      for (let index =536384; index < 759650; index++) {
-        url = 'http://www.wjlibw.space/mvtke_'+index+'.html'
-        await page.goto(url);  //371
-        let content = await page.content();
-        console.log(content);
-      }
-   
-
->>>>>>> 75473574eda27547eca51b348e0fd9b2722a5533
 //    $("li a ").each(function(i, e) {
 //        let url = $(e).attr("href");
 //     console.log($(e).text());
