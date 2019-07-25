f577b8e9a2ee441ded68b4c802e8b1ef8e931713
xzl <xzlperfect@163.com>
2019-07-25 15:17:42 +0800

    添加token 验证规则

--- app/middleware/loginAuth.js
+++ app/middleware/loginAuth.js
@@ -2,7 +2,7 @@
  * @Author: xzl 
  * @Date: 2019-07-22 14:05:18 
  * @Last Modified by: xzl
- * @Last Modified time: 2019-07-23 15:39:13
+ * @Last Modified time: 2019-07-25 15:16:53
  */
   // 管理端登录验证
   const jwt = require('jsonwebtoken');
@@ -11,7 +11,24 @@
   if(ctx.request.header['authorization']){  //当处于·登录状态时
     let userToken = ctx.request.header['authorization'];
     let decoded;
+     //验证token 是否为非法token
+     try {
+      decoded = jwt.verify(userToken, ctx.app.config.secrets) ;
+      let  verifyResult = await ctx.service.user.findbyUserByIdAndName(decoded.user_name,decoded.user_id);
+      if(!verifyResult){
+         ctx.helper.failure({ctx, code :500014 , message:'非法Token'})
+      }
 
+     } catch (error) {
+      if (error.name == 'TokenExpiredError') {  //token 令牌过期
+       //401 代表当前用户为登录
+      ctx.helper.failure({ctx, code :500012 , message:'登录过期'})
+
+      }
+
+     }
+   
+     
     next();
  }
  else{
--- app/service/user.js
+++ app/service/user.js
@@ -2,13 +2,18 @@
  * @Author: xzl 
  * @Date: 2019-07-19 14:18:14 
  * @Last Modified by: xzl
- * @Last Modified time: 2019-07-22 16:30:39
+ * @Last Modified time: 2019-07-25 15:13:47
  */
 const Service = require('egg').Service;
 class UserService  extends Service{
     async findbyUserName(username,role){   //根据用户名 和 角色查询信息
         const user  = await this.app.mysql.get('user',{name:username,role:role});
         return  user ;
+    }
+    //登录验证
+    async  findbyUserByIdAndName(username,id){
+        const verificationUser   =  await this.app.mysql.get('user',{name:username,id:id});
+        return verificationUser;
     }
 }
 
