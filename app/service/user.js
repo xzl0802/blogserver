596c7c398722be133189b4c98e936d416de346be
xzl <xzl@xzldeMacBook-Pro.local>
2019-11-08 21:50:18 +0800

    修改登陆逻辑

--- app/service/user.js
+++ app/service/user.js
@@ -5,14 +5,14 @@
  * @Last Modified time: 2019-10-15 15:32:20
  */
 const Service = require('egg').Service;
-class UserService  extends Service{
-    async findbyUserName(username,role){   //根据用户名 和 角色查询信息
-        const user  = await this.app.mysql.get('user',{user_id:username,role_code:role});
-        return  user ;
-    }
-    //登录验证
-    async  findbyUserByIdAndName(username,id){
-        const verificationUser   =  await this.app.mysql.get('user',{user_id:username,id:id});
+class UserService extends Service {
+    async findbyUserName(username, role) { //根据用户名 和 角色查询信息
+            const user = await this.app.mysql.get('user', { code: username });
+            return user;
+        }
+        //登录验证
+    async findbyUserByIdAndName(username, id) {
+        const verificationUser = await this.app.mysql.get('user', { user_id: username, id: id });
         return verificationUser;
     }
 }
