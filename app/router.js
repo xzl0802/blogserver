152ac68e7d411d7c3d4996f7526fff657a0c760e
xzl <xzlperfect@163.com>
2019-12-12 15:00:41 +0800

    调整文件层级

--- app/router.js
+++ app/router.js
@@ -10,12 +10,12 @@ module.exports = app => {
    router.get('/', controller.home.index);
   
    router.get('/image',controller.image.imageIndex);  //处理图片获取接口
-   router.post('/count/ip',controller.count.countIP); //统计ip 接口
+   router.get('/count/ip',controller.client.count.countIP); //统计ip 接口
    
    // #####################管理端接口######################
    router.post('/admin/login',controller.admin.login.loginIndex); //登录处理
    router.get('/admin/info',loginAuth,controller.admin.login.getUserInfo);//获取用户信息
-   router.post('/admin/count/ip',loginAuth,controller.count.getCountIpByPagination); //获取所有访问的Ip
+   router.post('/admin/count/ip',loginAuth,controller.client.count.getCountIpByPagination); //获取所有访问的Ip
    router.get('/admin/log',loginAuth,controller.admin.log.logQuery);//后台日志查询接口
 };
 
