82c2b700308d2bb8e3bb84bda1fa77468408e4c0
xzl <xzlperfect@163.com>
2019-12-26 16:38:55 +0800

    添加头像字段

--- app/service/admin/user.js
+++ app/service/admin/user.js
@@ -2,14 +2,14 @@
  * @Author: xzl 
  * @Date: 2019-07-19 14:18:14 
  * @Last Modified by: xzl
- * @Last Modified time: 2019-12-26 16:23:37
+ * @Last Modified time: 2019-12-26 16:36:10
  */
 const Service = require('egg').Service;
 class UserService extends Service {
     async getUserInfo(username, id) {
         const userInfo = await this.app.mysql.select('user', 
         {where:{code: username, id: id},
-         columns: ['name', 'role_code'],
+         columns: ['name', 'role_code','avatar'],
          limit: 1, // 返回数据量
          offset: 0,})
         return JSON.stringify(userInfo);
