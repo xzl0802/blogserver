139902bc00d7ba4a9b33e27815d3ceb70e0abbb8
xzl <xzlperfect@163.com>
2020-01-12 17:33:26 +0800

    w文章

--- app/controller/admin/article.js
+++ app/controller/admin/article.js
@@ -4,17 +4,28 @@
  * @Last Modified by: xzl
  * @Last Modified time: 2020-01-09 15:34:50
  */
-const Cotroller =  require('egg').Controller;
+const Cotroller = require('egg').Controller;
 
-class articleController  extends Cotroller{
-    
-  async getAllArticle(){ //获取所有的文章列表
-      
-  }
-    
+class articleController extends Cotroller {
 
+    async getAllArticle() { //获取所有的文章列表
+        let { ctx } = this;
+        let payload = ctx.request.body || {};
+        let page = payload.page - 1;
+        let start = payload.limit * page;
+        let limit = payload.limit;
+        let data = await ctx.service.admin.article.getAllArticleList(start, limit);
+        ctx.helper.success({ ctx, data });
+    }
+
+    async  getAllLabelList(){
+     let {ctx} = this;
+     let  data  =  await  ctx.service.admin.article.getAllLabels();
+     ctx.helper.success({ctx,data});
+
+    }
 
 }
 
 
-module.exports =articleController;
+module.exports = articleController;
--- app/router.js
+++ app/router.js
@@ -5,20 +5,22 @@
  */
 
 module.exports = app => {
-  const { router, controller,middlewares } = app;
-  const loginAuth = app.middlewares.loginAuth();  //管理端登录验证中间件
-   router.get('/', controller.home.index);
-  
-   router.get('/image',controller.image.imageIndex);  //处理图片获取接口
-   router.post('/count/ip',controller.client.count.countIP); //统计ip 接口
-   
-   // #####################管理端接口######################
-   router.post('/admin/login',controller.admin.login.loginIndex); //登录处理
-   router.get('/admin/user/info',loginAuth,controller.admin.user.getUserInfo);//获取用户信息
-   router.post('/admin/count/ip',loginAuth,controller.admin.log.getCountIpByPagination); //获取所有访问的Ip
-   router.post('/admin/label/query',loginAuth,controller.admin.label.getAllLabel);//文章标签查询接口
-   router.post('/admin/label/add',loginAuth,controller.admin.label.addLabel);// 文章标签新增接口
-   router.post('/admin/label/update',loginAuth,controller.admin.label.updateLabel);// 文章标签修改接口
-   router.get('/admin/label/delete/:id',loginAuth,controller.admin.label.deleteLabel);// 文章标签修改接口
+    const { router, controller, middlewares } = app;
+    const loginAuth = app.middlewares.loginAuth(); //管理端登录验证中间件
+    router.get('/', controller.home.index);
+
+    router.get('/image', controller.image.imageIndex); //处理图片获取接口
+    router.post('/count/ip', controller.client.count.countIP); //统计ip 接口
+
+    // #####################管理端接口######################
+    router.post('/admin/login', controller.admin.login.loginIndex); //登录处理
+    router.get('/admin/user/info', loginAuth, controller.admin.user.getUserInfo); //获取用户信息
+    router.post('/admin/count/ip', loginAuth, controller.admin.log.getCountIpByPagination); //获取所有访问的Ip
+    router.post('/admin/label/query', loginAuth, controller.admin.label.getAllLabel); //文章标签查询接口
+    router.post('/admin/label/add', loginAuth, controller.admin.label.addLabel); // 文章标签新增接口
+    router.post('/admin/label/update', loginAuth, controller.admin.label.updateLabel); // 文章标签修改接口
+    router.get('/admin/label/delete/:id', loginAuth, controller.admin.label.deleteLabel); // 文章标签修改接口
+    router.post('/admin/article/list', loginAuth, controller.admin.article.getAllArticle); //获取所有的文章
+    router.get('/admin.article/labels',loginAuth,controller.admin.article.getAllLabelList);//获取所用的标签
 
 };
